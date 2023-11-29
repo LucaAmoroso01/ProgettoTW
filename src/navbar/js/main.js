3680abddf2b98ae56ba760a3af24a4e43aebfb05
roccodelprete <122788299+roccodelprete@users.noreply.github.com>
2023-11-29 22:59:26 +0100

    make navbar responsive

--- src/navbar/js/main.js
+++ src/navbar/js/main.js
@@ -1,5 +1,32 @@
 const root = "/";
 
+const drivers = [
+  { firstName: "Charles", lastName: "Leclerc", team: "ferrari" },
+  { firstName: "Carlos", lastName: "Sainz", team: "ferrari" },
+  { firstName: "Lewis", lastName: "Hamilton", team: "mercedes" },
+  { firstName: "Valtteri", lastName: "Bottas", team: "alfa-romeo" },
+  { firstName: "Max", lastName: "Verstappen", team: "red-bull" },
+  { firstName: "Sergio", lastName: "Perez", team: "red-bull" },
+  { firstName: "Lando", lastName: "Norris", team: "mclaren" },
+  { firstName: "Oscar", lastName: "Piastri", team: "mclaren" },
+  { firstName: "Daniel", lastName: "Ricciardo", team: "alphaTauri" },
+  { firstName: "Pierre", lastName: "Gasly", team: "alpine" },
+  { firstName: "Esteban", lastName: "Ocon", team: "alpine" },
+  { firstName: "Lance", lastName: "Stroll", team: "aston-martin" },
+  { firstName: "Fernando", lastName: "Alonso", team: "aston-martin" },
+  { firstName: "George", lastName: "Russell", team: "mercedes" },
+  { firstName: "Yuki", lastName: "Tsunoda", team: "alphaTauri" },
+  { firstName: "Guanyu", lastName: "Zhou", team: "alfa-romeo" },
+  { firstName: "Alexander", lastName: "Albon", team: "williams" },
+  { firstName: "Kevin", lastName: "Magnussen", team: "haas" },
+  { firstName: "Nico", lastName: "Hulkenberg", team: "haas" },
+  { firstName: "Logan", lastName: "Sargeant", team: "williams" },
+].sort((a, b) => a.lastName.localeCompare(b.lastName));
+
+const teamsArray = drivers
+  .map((driver) => driver.team)
+  .sort((a, b) => a.localeCompare(b));
+
 export function loadPageContent(pageUrl, containerId) {
   fetch(pageUrl)
     .then((response) => response.text())
@@ -7,6 +34,12 @@ export function loadPageContent(pageUrl, containerId) {
       document.getElementById(containerId).innerHTML = html;
 
       loadTeamsAndDrivers();
+
+      window.addEventListener("resize", handleNavbarClass);
+
+      handleNavbarClass();
+
+      loadTeamsAndDriversResponsive(drivers, teamsArray);
 
       const loginButton = document.createElement("button");
       loginButton.classList.add("btn", "btn-primary", "btn-login-subscribe");
@@ -37,6 +70,20 @@ export function loadPageContent(pageUrl, containerId) {
       registrationButton.style.borderColor = "black";
       registrationButton.innerHTML = `registration`;
       registrationButton.addEventListener("click", () =>
+        openLoginOrRegistration(`/auth/login-registration?param=registration`)
+      );
+
+      const loginButtonResponsive = document.getElementById(
+        "login-button-responsive"
+      );
+      loginButtonResponsive.addEventListener("click", () =>
+        openLoginOrRegistration(`/auth/login-registration?param=login`)
+      );
+
+      const registrationButtonResponsive = document.getElementById(
+        "registration-button-responsive"
+      );
+      registrationButtonResponsive.addEventListener("click", () =>
         openLoginOrRegistration(`/auth/login-registration?param=registration`)
       );
 
@@ -81,10 +128,24 @@ export function loadPageContent(pageUrl, containerId) {
         driversDropdownContainer.style.cssText = "left: -2.8rem !important";
       }
 
-      openSchedule();
+      const scheduleButton = document.getElementById("schedule");
+      const scheduleResponsiveButton = document.getElementById(
+        "schedule-responsive"
+      );
+      handleStyleScheduleButton(scheduleButton);
+      handleStyleScheduleResponsiveButton(scheduleResponsiveButton);
 
       const userCard = document.getElementById("user-card");
+      userCard.style.display = "none";
       const userButton = document.getElementById("user-button");
+
+      const userCardResponsive = document.getElementById(
+        "user-card-responsive"
+      );
+      userCardResponsive.style.display = "none";
+      const userButtonResponsive = document.getElementById(
+        "user-button-responsive"
+      );
 
       fetch("/auth/user")
         .then((response) => {
@@ -98,19 +159,41 @@ export function loadPageContent(pageUrl, containerId) {
           if (data.status !== 401) {
             const user = data.user;
 
+            const dividerResponsive = document.getElementById(
+              "dropdown-divider-responsive"
+            );
+
             loginButton.style.display = "none";
             registrationButton.style.display = "none";
 
+            loginButtonResponsive.remove();
+            registrationButtonResponsive.remove();
+            dividerResponsive.remove();
+
+            document.getElementById("login-buttons-responsive").style.cssText =
+              "padding-bottom: 0 !important";
+
             userButton.style.display = "block";
+            userButtonResponsive.style.display = "block";
 
             userButton.addEventListener("click", () =>
               toggleUserCard(user, userButton, userCard)
             );
 
+            userButtonResponsive.addEventListener("click", () => {
+              toggleUserCard(user, userButtonResponsive, userCardResponsive);
+            });
+
             const logoutButton = document.getElementById("logout-button");
             logoutButton.addEventListener("click", () => logout());
           }
         });
+
+      const homeResponsiveButton = document.getElementById("home-responsive");
+
+      if (window.location.pathname === "/") {
+        homeResponsiveButton.remove();
+      }
     })
     .catch((error) => console.error("Error loading page content:", error));
 }
@@ -128,18 +211,7 @@ function openTeam(teamToOpen) {
 }
 
 function openSchedule() {
-  if (window.location.pathname === "/schedule") {
-    const scheduleButton = document.getElementById("schedule");
-    scheduleButton.classList.add("nav-link-active");
-
-    scheduleButton.addEventListener("mouseenter", () => {
-      scheduleButton.classList.add("nav-link-hover");
-    });
-
-    scheduleButton.addEventListener("mouseleave", () => {
-      scheduleButton.classList.remove("nav-link-hover");
-    });
-  }
+  window.location.href = `/schedule`;
 }
 
 function loadTeamsAndDrivers() {
@@ -256,6 +328,89 @@ function loadTeamsAndDrivers() {
   });
 }
 
+function loadTeamsAndDriversResponsive(drivers, teamsArray) {
+  function createDriverListItem(driver) {
+    const listItem = document.createElement("button");
+    listItem.id = driver.lastName.toLowerCase();
+
+    listItem.classList.add("dropdown-item", "dropdown-item-responsive");
+    listItem.style.cssText = "padding: 0rem !important";
+
+    listItem.addEventListener("click", () =>
+      openDriver(driver.lastName.toLowerCase())
+    );
+
+    const formattedName = `
+                            <div style="display: flex !important; align-content: center !important; margin-top: 1rem; gap: 10px; padding-inline: 2rem">
+                              <i class="fa-sharp fa-light fa-rectangle-wide driver-${
+                                driver.team
+                              }-color"></i>
+                              <div class="driver-name">
+                                <p style="font-family: F1 Regular">
+                                  ${driver.firstName}
+                                </p>
+                                <p>${driver.lastName.toUpperCase()}</p>
+                              </div>
+                            </div>`;
+
+    listItem.innerHTML = formattedName;
+    return listItem;
+  }
+
+  function createTeamListItem(team) {
+    const listItem = document.createElement("button");
+    listItem.classList.add("dropdown-item", "dropdown-item-responsive");
+    listItem.addEventListener("click", () => openTeam(team));
+
+    const formattedTeamName = team
+      .replace("-", " ")
+      .replace(/\b\w/g, (char) => char.toUpperCase());
+
+    const formattedName = `<div style="display: flex !important; align-content: center !important; margin-top: 2px; padding-inline: 2rem !important">
+                              <p style="font-family: F1 Regular; margin-top: 1rem">
+                                ${formattedTeamName}
+                              </p>
+                            </div>`;
+
+    listItem.innerHTML = formattedName;
+
+    return listItem;
+  }
+
+  const driverListContainer = document.getElementById(
+    "drivers-list-responsive"
+  );
+
+  drivers.forEach((driver) => {
+    const listItem = createDriverListItem(driver);
+
+    if (
+      window.location.href.split("/").pop().replace(".html", "") ===
+      driver.lastName.toLowerCase()
+    ) {
+      listItem.style.cssText = "background-color: red !important";
+    }
+
+    driverListContainer.appendChild(listItem);
+  });
+
+  const teamsListContainer = document.getElementById("teams-list-responsive");
+  const addedTeams = [];
+
+  teamsArray.forEach((team) => {
+    if (!addedTeams.includes(team)) {
+      const listItemTeam = createTeamListItem(team);
+
+      if (window.location.href.split("/").pop().replace(".html", "") === team) {
+        listItemTeam.style.cssText = "background-color: red !important";
+      }
+
+      teamsListContainer.appendChild(listItemTeam);
+      addedTeams.push(team);
+    }
+  });
+}
+
 function toggleUserCard(user, userButton, userCard) {
   userCard.style.display = userCard.style.display === "none" ? "block" : "none";
 
@@ -264,9 +419,22 @@ function toggleUserCard(user, userButton, userCard) {
   const userFirstName = document.getElementById("user-firstName");
   const userLastName = document.getElementById("user-lastName");
   const userEmail = document.getElementById("user-email");
+
   userFirstName.innerHTML = user.firstName;
   userLastName.innerHTML = user.lastName;
   userEmail.innerHTML = user.email;
+
+  const userFirstNameResponsive = document.getElementById(
+    "user-firstName-responsive"
+  );
+  const userLastNameResponsive = document.getElementById(
+    "user-lastName-responsive"
+  );
+  const userEmailResponsive = document.getElementById("user-email-responsive");
+
+  userFirstNameResponsive.innerHTML = user.firstName;
+  userLastNameResponsive.innerHTML = user.lastName;
+  userEmailResponsive.innerHTML = user.email;
 }
 
 function logout() {
@@ -285,4 +453,83 @@ function logout() {
         window.location.href = "/";
       }
     });
+}
+
+function handleNavbarClass() {
+  const navbar = document.getElementById("navbar");
+  const responsiveNavbar = document.getElementById("responsive-navbar");
+
+  if (window.innerWidth <= 1050) {
+    navbar.classList.remove("navbar-expand-lg");
+    responsiveNavbar.style.display = "flex";
+  } else {
+    navbar.classList.add("navbar-expand-lg");
+    responsiveNavbar.style.display = "none";
+  }
+
+  if (responsiveNavbar.style.display === "flex") {
+    navbar.style.cssText = "padding: 0 !important; margin: 0 !important";
+
+    const driversResponsiveButton = document.getElementById(
+      "drivers-responsive-button"
+    );
+
+    const teamsResponsiveButton = document.getElementById(
+      "teams-responsive-button"
+    );
+
+    const submenu = document.getElementById("dropdown-drivers-submenu");
+    submenu.style.cssText = "display: none !important";
+
+    const teamsSubmenu = document.getElementById("dropdown-teams-submenu");
+    submenu.style.cssText = "display: none !important";
+
+    driversResponsiveButton.addEventListener("click", () => {
+      if (submenu.style.display === "block") {
+        submenu.style.cssText = "display: none !important";
+      } else {
+        submenu.style.cssText = "display: block !important";
+      }
+    });
+
+    teamsResponsiveButton.addEventListener("click", () => {
+      if (teamsSubmenu.style.display === "block") {
+        teamsSubmenu.style.cssText = "display: none !important";
+      } else {
+        teamsSubmenu.style.cssText = "display: block !important";
+      }
+    });
+
+    const menuButton = document.getElementById("menu-button");
+    const menu = document.getElementById("dropdown-responsive");
+    menu.style.display = "none";
+
+    menuButton.addEventListener("click", (e) => {
+      menu.style.display = menu.style.display === "none" ? "block" : "none";
+    });
+  }
+}
+
+function handleStyleScheduleButton(scheduleButton) {
+  scheduleButton.addEventListener("click", openSchedule);
+
+  if (window.location.pathname === "/schedule") {
+    scheduleButton.classList.add("nav-link-active");
+
+    scheduleButton.addEventListener("mouseenter", () => {
+      scheduleButton.classList.add("nav-link-hover");
+    });
+
+    scheduleButton.addEventListener("mouseleave", () => {
+      scheduleButton.classList.remove("nav-link-hover");
+    });
+  }
+}
+
+function handleStyleScheduleResponsiveButton(scheduleButton) {
+  scheduleButton.addEventListener("click", openSchedule);
+
+  if (window.location.pathname === "/schedule") {
+    scheduleButton.style.cssText = "background-color: red !important";
+  }
 }
